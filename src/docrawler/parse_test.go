b01fcb2132fa85537dd5d99883226ce35174e48f
Stephen Waits <steve@waits.net>
2015-02-15 08:03:19 -0800

    add parsing of <title>...</title> to parse()

--- src/docrawler/parse_test.go
+++ src/docrawler/parse_test.go
@@ -4,19 +4,23 @@ import (
 	"testing"
 )
 
-// TestSimpleParse verifies that we can extract URLs (etc.) from a known document
+// TestSimpleParse verifies that we can extract the page Title and all URLs (etc.) from a known document
 func TestSimpleParse(t *testing.T) {
 	doc := `<html>
 	<head>
+		<title> Test Page  </title>
 	</head>
 	<body>
 		<img src="/assets/image.png"/>
 		<a href="/about.html">
 	</body>
 </html>`
-	matches, err := parse(doc)
+	title, matches, err := parse(doc)
 	if err != nil {
 		t.Error(err)
+	}
+	if title != "Test Page" {
+		t.Error("got wrong title")
 	}
 	if len(matches) != 2 {
 		t.Error("invalid number of matches in parse")
